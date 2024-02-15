d9bcbd737ddb8f6d4d7cc6e5ebf7af3b4aa4f902
rosaSchutz <rosschutz1997@gmail.com>
2024-02-15 16:21:13 -0500

    change ccs gallery images

--- src/Routes/GalleryImages.jsx
+++ src/Routes/GalleryImages.jsx
@@ -1,71 +1,112 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/galleryImages.module.css";
 
 const GalleryImages = () => {
     return (
-
-        <div class="relative z-10" role="dialog" aria-modal="true">
-
-            <div class="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block"></div>
-
-            <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-                <div class="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
-
-                    <div class="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
-                        <div class="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
-                            <button type="button" class="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8">
-                                <span class="sr-only">Close</span>
-                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        <div className={styles.gallery}>
+                <div className={styles.galleryDiv1}>
+                    <div className={styles.galleryDiv2}>
+                        <div className={styles.galleryDiv3}>
+                            <Link to="/product/20">
+                            <button
+                                type="button"
+                                className={styles.button}
+                            >   
+                                <svg
+                                    className={styles.close}
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
                                 </svg>
                             </button>
-                            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="" />
-                                </div>
-                                <div>
-                                    <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="" />
-                                </div>
+                            </Link>
+                            <div className={styles.containerImg}>
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+                                    alt=""
+                                />
+
+                                <img
+                                    className={styles.img}
+                                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+                                    alt=""
+                                />
                             </div>
-
-
-
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
+    );
+};
 
-    )
-}
-
-export default GalleryImages
+export default GalleryImages;
