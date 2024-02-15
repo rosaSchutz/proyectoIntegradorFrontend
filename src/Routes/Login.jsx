6b7f8e31d6a918945edfdc862939a6f9ebb16b9c
rosaSchutz <rosschutz1997@gmail.com>
2024-02-15 06:33:20 -0500

    change css login and register

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -8,26 +8,26 @@ const Login = () => {
                 <img className={styles.login_img} src="/assets/logo.png" alt="" />
                 <h2 className={styles.login_subtitle}>Iniciar sesión</h2>
             </div>
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                <form class="space-y-6" action="#" method="POST">
+            <div className={styles.containerForm}>
+                <form className={styles.input_form} action="#" method="POST">
                     <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-                        <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <label for="email" className={styles.input_label}>Email</label>
+                        <div className={styles.div}>
+                            <input id="email" name="email" type="email" autocomplete="email" required className={styles.input} />
                         </div>
                     </div>
 
                     <div>
-                        <div class="flex items-center justify-between">
-                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
+                        <div className={styles.div_password}>
+                            <label for="password" className={styles.input_label}>Contraseña</label>
                         </div>
-                        <div class="mt-2">
-                            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <div className={styles.div}>
+                            <input id="password" name="password" type="password" autocomplete="current-password" required className={styles.input} />
                         </div>
                     </div>
 
                     <div>
-                        <button type="submit" class="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm border">Iniciar sesión</button>
+                        <button type="submit" className={styles.button} >Iniciar sesión</button>
                     </div>
                 </form>
             </div>
--- src/Routes/Register.jsx
+++ src/Routes/Register.jsx
@@ -6,28 +6,28 @@ const Register = () => {
         <article className={styles.container_login}>
             <div className={styles.login_title}>
                 <img className={styles.login_img} src="/assets/logo.png" alt="" />
-                <h2 className={styles.login_subtitle}>Iniciar sesión</h2>
+                <h2 className={styles.login_subtitle}>Crear cuenta</h2>
             </div>
-            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-                <form class="space-y-6" action="#" method="POST">
+            <div className={styles.containerForm}>
+                <form className={styles.input_form} action="#" method="POST">
                     <div>
-                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-                        <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <label for="email" className={styles.input_label}>Email</label>
+                        <div className={styles.div}>
+                            <input id="email" name="email" type="email" autocomplete="email" required className={styles.input} />
                         </div>
                     </div>
 
                     <div>
-                        <div class="flex items-center justify-between">
-                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Contraseña</label>
+                        <div className={styles.div_password}>
+                            <label for="password" className={styles.input_label}>Contraseña</label>
                         </div>
-                        <div class="mt-2">
-                            <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <div className={styles.div}>
+                            <input id="password" name="password" type="password" autocomplete="current-password" required className={styles.input} />
                         </div>
                     </div>
 
                     <div>
-                        <button type="submit" class="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm border">Iniciar sesión</button>
+                        <button type="submit" className={styles.button} >Crear cuenta</button>
                     </div>
                 </form>
             </div>
